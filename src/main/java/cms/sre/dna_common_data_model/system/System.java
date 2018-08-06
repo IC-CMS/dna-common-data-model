a5a6f588448458d3aa739f9a4b9b3d4cacc1463a
campbelldanieldc3 <40569039+campbelldanieldc3@users.noreply.github.com>
2018-08-06 12:55:49 -0400

    Update System.java

--- src/main/java/cms/sre/dna_common_data_model/system/System.java
+++ src/main/java/cms/sre/dna_common_data_model/system/System.java
@@ -2,7 +2,27 @@ package cms.sre.dna_common_data_model.system;
 
 import java.util.List;
 import java.util.Map;
+import java.util.ArrayList;
 import java.util.Objects;
+import cms.sre.dna_common_data_model.system.Toaster;
+import cms.sre.dna_common_data_model.hashicorpFile.PackerScript;
+import cms.sre.dna_common_data_model.hashicorpFile.ScriptFile;
+import cms.sre.dna_common_data_model.hashicorpFile.TerraformScript;
+import cms.sre.dna_common_data_model.system.PersistentVolume;
+
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.KeyDeserializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 public class System {
     private String name;
@@ -73,5 +93,165 @@ public class System {
     public int hashCode() {
 
         return Objects.hash(name, description, owner, toasters, dependenciesMap);
+    }
+}
+
+/**
+*
+* Deserialization Classes
+*
+**/
+public class CustomKeyDeserializer extends KeyDeserializer {
+
+    private Logger logger = LoggerFactory.getLogger(CustomKeyDeserializer.class);
+
+    @Override
+    public Toaster deserializeKey(String key, DeserializationContext ctxt) throws IOException{
+
+        logger.info("Custom Key Deserialization has been accessed.");
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule();
+
+        module.addDeserializer(Toaster.class, new ToasterDeserializer());
+
+        return mapper.readValue(key, Toaster.class);
+
+    }
+}
+/**
+*
+*
+*
+**/
+public class ToasterDeserializer extends StdDeserializer<Toaster> {
+
+    public ToasterDeserializer(){
+        this(null);
+    }
+
+    public ToasterDeserializer(Class<?> vc){
+
+        super(vc);
+
+    }
+
+    @Override
+    public Toaster deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+
+        Toaster mmmToast = new Toaster();
+        JsonNode rootNode = jp.getCodec().readTree(jp);
+
+        List<PersistentVolume> volumes = new ArrayList<>();
+
+        for(JsonNode node : rootNode.get("persistentVolumes")){
+
+            volumes.add(
+                    new PersistentVolume()
+                            .setId(node.get("id").asText())
+                            .setName(node.get("name").asText())
+                            .setSpaceInGb(node.get("spaceInGb").asInt()));
+        }
+
+        mmmToast.setPackerScript(
+                new PackerScript()
+                        .setScriptFile(
+                                new ScriptFile()
+                                        .setFilename(
+                                                rootNode
+                                                        .get("packerScript")
+                                                        .get("scriptFile")
+                                                        .get("fileName")
+                                                        .asText())
+                                        .setBinaryFile(
+                                                rootNode.get("packerScript")
+                                                .get("scriptFile")
+                                                .get("binaryFile")
+                                                .binaryValue())
+                                        .setContents(
+                                                rootNode.get("packerScript")
+                                                        .get("scriptFile")
+                                                        .get("contents")
+                                                        .asText())))
+                .setTerraformScript(
+                        new TerraformScript()
+                                .setMainScript(
+                                        new ScriptFile()
+                                        .setFilename(
+                                                rootNode
+                                                        .get("terraformScript")
+                                                        .get("scriptFile")
+                                                        .get("fileName")
+                                                        .asText()
+                                        )
+                                .setBinaryFile(
+                                        rootNode.get("terraformScript")
+                                                .get("scriptFile")
+                                                .get("binaryFile")
+                                                .binaryValue()
+                                ).setContents(
+                                                rootNode.get("terraformScript")
+                                                        .get("scriptFile")
+                                                        .get("contents")
+                                                        .asText()))
+                                .setVariableScript(
+                                new ScriptFile()
+                                        .setFilename(
+                                                rootNode
+                                                        .get("terraformScript")
+                                                        .get("scriptFile")
+                                                        .get("fileName")
+                                                        .asText()
+                                        )
+                                        .setBinaryFile(
+                                                rootNode.get("terraformScript")
+                                                        .get("scriptFile")
+                                                        .get("binaryFile")
+                                                        .binaryValue()
+                                        ).setContents(
+                                        rootNode.get("terraformScript")
+                                                .get("scriptFile")
+                                                .get("contents")
+                                                .asText()))
+                                .setProviderScript(
+                                new ScriptFile()
+                                        .setFilename(
+                                                rootNode
+                                                        .get("terraformScript")
+                                                        .get("scriptFile")
+                                                        .get("fileName")
+                                                        .asText()
+                                        )
+                                        .setBinaryFile(
+                                                rootNode.get("terraformScript")
+                                                        .get("scriptFile")
+                                                        .get("binaryFile")
+                                                        .binaryValue()
+                                        ).setContents(
+                                        rootNode.get("terraformScript")
+                                                .get("scriptFile")
+                                                .get("contents")
+                                                .asText()))
+                                .setDataSourcesScript(
+                                new ScriptFile()
+                                        .setFilename(
+                                                rootNode
+                                                        .get("terraformScript")
+                                                        .get("scriptFile")
+                                                        .get("fileName")
+                                                        .asText()
+                                        )
+                                        .setBinaryFile(
+                                                rootNode.get("terraformScript")
+                                                        .get("scriptFile")
+                                                        .get("binaryFile")
+                                                        .binaryValue()
+                                        ).setContents(
+                                        rootNode.get("terraformScript")
+                                                .get("scriptFile")
+                                                .get("contents")
+                                                .asText())))
+                .setPersistentVolumes(volumes);
+
+        return mmmToast;
     }
 }
