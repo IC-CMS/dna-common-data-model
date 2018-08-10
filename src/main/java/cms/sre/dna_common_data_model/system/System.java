3bb1561249ab186f3ba74601dff4c95d1a1ec839
David Hessler <davidh.092705@gmail.com>
2018-08-10 10:18:39 -0400

    Include Jackson-databind into pom and clean up compliation error.

--- src/main/java/cms/sre/dna_common_data_model/system/System.java
+++ src/main/java/cms/sre/dna_common_data_model/system/System.java
@@ -14,6 +14,7 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.KeyDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,7 +23,7 @@ import java.io.IOException;
 public class System {
     protected class ToasterKeyDeserializer extends KeyDeserializer {
 
-        private Logger logger = LoggerFactory.getLogger(CustomKeyDeserializer.class);
+        private Logger logger = LoggerFactory.getLogger(ToasterKeyDeserializer.class);
 
         @Override
         public Toaster deserializeKey(String key, DeserializationContext ctxt) throws IOException{
@@ -107,12 +108,5 @@ public class System {
         return Objects.hash(name, description, owner, toasters, dependenciesMap);
     }
 }
-
-/**
-*
-* Key Deserialization Class
-*
-**/
-
 
 
