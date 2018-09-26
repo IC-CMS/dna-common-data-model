098d0ef1297256bfab8a59a64f4b7f8a228a7334
ermoffa <edward.moffatt@markpointech.com>
2018-09-26 16:42:39 +0000

    Added a uuid and createdDate member variables to the class.

--- src/main/java/cms/sre/dna_common_data_model/emailnotifier/Email.java
+++ src/main/java/cms/sre/dna_common_data_model/emailnotifier/Email.java
@@ -1,19 +1,47 @@
 package cms.sre.dna_common_data_model.emailnotifier;
 
+import java.util.Date;
+import java.util.UUID;
+
 /**
  * Common Email class used between micro services
  */
 public class Email {
 
+    private Date createdDate;
+    private String uuid;
     private String emailAddress;
     private String subject;
     private String body;
 
+    public Email () {
+
+        this.uuid = UUID.randomUUID().toString();
+
+    }
+
     public Email(String emailAddress, String subject, String body) {
 
+        this.uuid = UUID.randomUUID().toString();
+        this.emailAddress = emailAddress;
+        this.subject = subject;
+        this.body = body;
+    }
+
+    public Email(String uuid, String emailAddress, String subject, String body) {
+
+        this.uuid = UUID.randomUUID().toString();
         this.emailAddress = emailAddress;
         this.subject = subject;
         this. body = body;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
     }
 
     public String getEmailAddress() {
@@ -43,10 +71,23 @@ public class Email {
         return this;
     }
 
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        // If already set, don't allow changing it
+        if (this.uuid == null ) {
+            this.uuid = uuid;
+        }
+    }
+
     @Override
     public String toString() {
         return "Email{" +
-                "emailAddress='" + emailAddress + '\'' +
+                "createdDate=" + createdDate +
+                ", uuid='" + uuid + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
                 ", subject='" + subject + '\'' +
                 ", body='" + body + '\'' +
                 '}';
